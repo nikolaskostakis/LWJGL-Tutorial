515f407af8dff98aec7ff2ad0ea6d4dcbcaa3856
Nikolas <Kostakis>
2020-06-05 21:19:09 +0300

    Added Window to the game
    This project is based on the Youtuber CodingAP series about LWJGL 3
    
    The link to the second video:
    https://www.youtube.com/watch?v=QtUYQ92anG4

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,43 +5,54 @@
  */
 package main;
 
+import engine.io.Window;
+
 /**
  *
- * @author User
+ * @author Nikolas Kostakis
  */
-public class Main implements Runnable {
+public class Main implements Runnable{
 
     public Thread game;
-    
+    public static Window window;
+    public static final int WIDTH = 1280, HEIGHT = 760;
+
     public void start() {
         this.game = new Thread(this, "game");
         this.game.start();
     }
     
-    public static void init() {
+    public static void init(){
         System.out.println("Initializing Game!");
+        
+        // Create the window
+        window = new Window(Main.WIDTH, Main.HEIGHT, "Game");
+        window.create();
     }
     
-    public void run() {
+    public void run(){
         Main.init();
-        while (true) {
-            update();
-            render();
+        while (!window.shouldClose()){
+            this.update();
+            this.render();
         }
     }
     
     private void update(){
-        System.out.println("Updating Game!");
+        // System.out.println("Updating Game!");
+        window.update();
     }
     
     private void render(){
-        System.out.println("Rendering Game!");
+        // System.out.println("Rendering Game!");
+        window.swapBuffers();
+        
     }
     
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         new Main().start();
     }
     
---
+++ src/engine/io/Window.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package engine.io;
+
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.glfw.GLFWWindowPosCallback;
+
+/**
+ *
+ * @author Nikolas Kostakis
+ */
+public class Window {
+
+    private final int width, height; // Width & Height of the window 
+    private final String title; // Title of the window
+    private long windowVar;
+    private int frames;
+    private static long time;
+    
+    /**
+     * Constructor 
+     * @param width
+     * @param height
+     * @param title 
+     */
+    public Window(int width, int height, String title){
+        this.width = width;
+        this.height = height;
+        this.title = title;
+    }
+    
+    public void create(){
+        if (!GLFW.glfwInit()) {
+            System.err.println("ERROR: GLFW was not initialized!");
+            return;
+        }
+        
+        // Create window
+        this.windowVar = GLFW.glfwCreateWindow(this.width, this.height, this.title, 0, 0);
+        
+        // Check if the window was created
+        if (this.windowVar == 0){
+            System.err.println("ERROR: Window was not created!");
+            return;
+        }
+        
+        // Set postion of window to the center of the primary monitor
+        GLFWVidMode videoMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+        GLFW.glfwSetWindowPos(this.windowVar, ((videoMode.width() - this.width) / 2), ((videoMode.height() - this.height) / 2));
+        
+        // Make the window the one GLFW changes are made
+        GLFW.glfwMakeContextCurrent(this.windowVar);
+        
+        // Show the window
+        GLFW.glfwShowWindow(this.windowVar);
+        
+        // Determine the frame rate to 60FPS !?
+        // TODO check for different frame rates
+        GLFW.glfwSwapInterval(1);
+        
+        // Get the current time
+        Window.time = System.currentTimeMillis();
+    }
+    
+    public void update(){
+        // Gets all the callbacks connected to this window
+        GLFW.glfwPollEvents();
+        
+        // Update the frame rate
+        this.frames++;
+        
+        // Check if the time has passed
+        // It counts in milliseconds
+        if (System.currentTimeMillis() > (Window.time + 1000)){
+            // Print frames into the title
+            GLFW.glfwSetWindowTitle(this.windowVar, (this.title + " | FPS:" + this.frames));
+            
+            Window.time = System.currentTimeMillis();
+            this.frames = 0;
+        }
+    }
+    
+    public void swapBuffers(){
+        // Swaps buffers of window
+        GLFW.glfwSwapBuffers(this.windowVar);
+    }
+    
+    public boolean shouldClose(){
+        return GLFW.glfwWindowShouldClose(this.windowVar);
+    }
+}
